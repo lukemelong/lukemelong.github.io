a09ec4a0ec65791af4d96a53f1d5a57d7912c9af
Luke Melong <lukemelong@protonmail.com>
2022-09-25 22:47:56 -0400

    Added dark mode switch

--- src/App.js
+++ src/App.js
@@ -1,20 +1,54 @@
 // Main
 import { useEffect, useState } from 'react'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 // Components
 import Container from '@mui/material/Container'
 import ScoreCard from './components/ScoreCard'
 import Stack from '@mui/material/Stack'
+import Switch from '@mui/material/Switch'
 // Utils
 import { getScoreData } from './utils'
+import { dark } from '@mui/material/styles/createPalette';
+import { Box } from '@mui/system';
+import { Grid, Typography } from '@mui/material';
 
 const App = () => {
-
+  // Styles
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+  const lightTheme = createTheme({
+      palette: {
+        mode: 'light',
+      },
+  })
+  // State
   const [loading, setLoading] = useState(false)
   const [scoreData, setScoreData] = useState()
-
+  const [isDarkMode, setIsDarkMode] = useState(false)
+  // Input Props
+  const darkModeSwitchProps = {
+    inputProps: {
+      'aria-label': 'Dark Mode'
+    }
+  }
+  // Event listeners
+  const darkModeSwitchOnClick = () => {
+    setIsDarkMode(!isDarkMode)
+  }
+  // Effects
+  // Get sports scores data (currently only for NFL games)
   useEffect(() => {
+    setIsDarkMode(localStorage.getItem('darkMode'))
     getScoreData(setLoading, setScoreData)
   }, [])
+  // Set theme preference in localstorage
+  useEffect(() => {
+    localStorage.setItem('darkMode', '' + isDarkMode)
+  }, [isDarkMode])
 
   // TODO Data isn't returned yet, need to make this prettier
   if(loading) {
@@ -24,27 +58,40 @@ const App = () => {
   }
 
   return (
-      <Container
-      maxWidth="lg"
+      <ThemeProvider
+      theme={isDarkMode ? darkTheme  : lightTheme}
       >
-        <Stack
-        direction="row"
-        gap={2}
-        sx={{
-          flexWrap: "wrap",
-          justifyContent: "center"
-        }}
+        <CssBaseline />
+        <Container
+        maxWidth="lg"
         >
-          {scoreData?.map(game => 
-            (
-              <ScoreCard
-              game={game}
-              key={game.id}
-              />
-            )
-          )}
-        </Stack>
-      </Container>
+          <Box>
+            <Typography display="inline">Dark Mode</Typography>
+            <Switch
+            {...darkModeSwitchProps}
+            onClick={darkModeSwitchOnClick}
+            defaultChecked={isDarkMode}
+            />
+          </Box>
+          <Stack
+          direction="row"
+          gap={2}
+          sx={{
+            flexWrap: "wrap",
+            justifyContent: "center"
+          }}
+          >
+            {scoreData?.map(game => 
+              (
+                <ScoreCard
+                game={game}
+                key={game.id}
+                />
+              )
+            )}
+          </Stack>
+        </Container>
+      </ThemeProvider>
   );
 }
 
